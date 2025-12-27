bf17d9323cd38aa30715d68de07da634f067733b
Shevaaniarun <shevaaniarun@gmail.com>
2025-12-27 18:36:05 +0530

    Changed path for index.ts

---
+++ Frontend/src/index.ts
@@ -0,0 +1,21 @@
+/**
+ * NyayaNet Components Export
+ * Central export file for all application components
+ */
+
+export { Sidebar } from './components/Sidebar';
+export { PostCard } from './components/PostCard';
+export { CaseCard } from './components/CaseCard';
+export { AIAssistant } from './components/AIAssistant';
+export { JusticeLoader } from './components/JusticeLoader';
+export { Header } from './components/Header';
+export { CreatePost } from './components/CreatePost';
+export { MobileNotice } from './components/MobileNotice';
+export { DiscussionCard } from './components/DiscussionCard';
+export { DiscussionDetail } from './components/DiscussionDetail';
+export { CreateDiscussion } from './components/CreateDiscussion';
+export { DiscussionFilters } from './components/DiscussionFilters';
+
+export type { Post } from './components/PostCard';
+export type { CaseItem } from './components/CaseCard';
+export type { Discussion } from './components/DiscussionCard';
